144f9b140d958940bca133c83eaacf91c24b2c1f
Lino09 <onosmx@gmail.com>
2022-01-12 14:31:08 -0600

    Fix data creation

--- data.sql
+++ data.sql
@@ -1,16 +1,16 @@
 /* Populate database with sample data. */
 --Neutered uses a bit 0 for false and 1 for true
 
-INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weigth_kg) VALUES (1 ,'Agumon',DATE '2020-02-03',0 , B'1', 10.23);
-INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weigth_kg) VALUES (2 ,'Gabumon',DATE '2018-11-15',2 , B'1', 8);
-INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weigth_kg) VALUES (3 ,'Pikachu',DATE '2020-01-07',1 , B'0', 15.04);
-INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weigth_kg) VALUES (4 ,'Devimon',DATE '2017-05-12',5 , B'1', 11);
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weigth_kg) VALUES ('Agumon',DATE '2020-02-03',0 , B'1', 10.23);
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weigth_kg) VALUES ('Gabumon',DATE '2018-11-15',2 , B'1', 8);
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weigth_kg) VALUES ('Pikachu',DATE '2020-01-07',1 , B'0', 15.04);
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weigth_kg) VALUES ('Devimon',DATE '2017-05-12',5 , B'1', 11);
 
 --day 2
-INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weigth_kg) VALUES (5 ,'Charmander',DATE '2020-02-08',0 , B'0', -11);
-INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weigth_kg) VALUES (6 ,'Plantmon',DATE '2022-11-15',2 , B'1', -5.7);
-INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weigth_kg) VALUES (7 ,'Squirtle',DATE '1993-04-02',3 , B'0', -12.13);
-INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weigth_kg) VALUES (8 ,'Angemon',DATE '2005-05-12',1 , B'1', -45);
-INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weigth_kg) VALUES (9 ,'Boarmon',DATE '2005-05-07',7 , B'1', 20.4);
-INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weigth_kg) VALUES (10 ,'Blossom',DATE '1998-10-13',3 , B'1', 17);
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weigth_kg) VALUES ('Charmander',DATE '2020-02-08',0 , B'0', -11);
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weigth_kg) VALUES ('Plantmon',DATE '2022-11-15',2 , B'1', -5.7);
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weigth_kg) VALUES ('Squirtle',DATE '1993-04-02',3 , B'0', -12.13);
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weigth_kg) VALUES ('Angemon',DATE '2005-05-12',1 , B'1', -45);
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weigth_kg) VALUES ('Boarmon',DATE '2005-05-07',7 , B'1', 20.4);
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weigth_kg) VALUES ('Blossom',DATE '1998-10-13',3 , B'1', 17);
 
--- schema.sql
+++ schema.sql
@@ -3,12 +3,13 @@ CREATE DATABASE vet_clinic;
 -- move to the database using \c vet_clinic
 
 CREATE TABLE animals( 
-  id INT,
+  id INT GENERATED ALWAYS AS IDENTITY,
   name VARCHAR(200),
   date_of_birth DATE,
   escape_attempts INT,
   neutered BIT,
-  weigth_kg DECIMAL
+  weigth_kg DECIMAL,
+  PRIMARY KEY(id)
 );
 
 --day 2
